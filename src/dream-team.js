a366d68f12ec4b77437c89e4d09642ec926685eb
alpekhota <alekspekhota@gmail.com>
2022-04-29 18:43:12 +0200

    feat: add decision for tast count-cats and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  let dreamTeam = [];
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(!Array.isArray(members)){
+    return false;
+  }
+
+
+  for(let i = 0; i< members.length; i++){
+    if(typeof members[i] === 'string'){
+      members[i] = members[i].replace(/\s+/g, '');
+    }
+    if(typeof members[i] === 'string'){
+      dreamTeam.push(members[i][0].toUpperCase());
+    }
+  }
+  return dreamTeam.sort().join('');
 }
 
 module.exports = {
