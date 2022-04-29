14f32e1738932dc821a8aadce8d7aebf6a2505d9
alpekhota <alekspekhota@gmail.com>
2022-04-29 21:50:33 +0200

    feat: add decision to what-seasom task

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+    
+    if(date == undefined){
+      return 'Unable to determine the time of year!';
+    }
+   
+
+    if(!(date instanceof Date && !isNaN(date))){
+      throw Error('Invalid date!');
+    }
+
+    // if(!(date instanceof Date)){
+    //   throw Error('Invalid date!');
+    // }
+    
+    
+    // if (date.toISOString === undefined){
+    //   throw Error('Invalid date!');
+    // }
+    // if (Object.getOwnPropertyNames(Date()) != Object.getOwnPropertyNames(date)){
+    //   throw Error('Invalid date!');
+    // }
+    // if (Object.getOwnPropertyNames(Date()) !== Object.getOwnPropertyNames(date)){
+    //   throw Error('Invalid date!');
+    // }
+    
+    let month = date.getMonth();
+    if(month < 2 || month == 11){
+      return 'winter';
+    } else if(month >=2 && month < 5 ){
+      return 'spring';
+    }else if(month >=5 && month < 8 ){
+      return 'summer';
+    }else if(month >=8 && month < 11 ){
+      return 'autumn';
+    }
+    
 }
 
 module.exports = {
