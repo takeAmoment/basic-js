2c226a37be3bb48ee50508ae64ad81e9f50d9caa
alpekhota <alekspekhota@gmail.com>
2022-04-30 11:44:52 +0200

    feat: add sunction getMatrixElementsSum to matrix-elements-sum task

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function getMatrixElementsSum(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let newArr = matrix[0].map((col,i) => matrix.map(row => row[i]));
+  let sum = 0;
+  for(let i = 0; i < newArr.length; i++ ){
+    for(let j = 0; j < newArr[i].length; j++){
+      if(newArr[i][j] === 0){
+        break;
+      } else{
+        sum += newArr[i][j];
+      }
+    }
+  }
+  return sum;
 }
 
 module.exports = {
