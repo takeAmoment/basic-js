6150fe1882ce73ad1d05bbe64cc10acf78159b42
alpekhota <alekspekhota@gmail.com>
2022-04-30 18:34:11 +0200

    feat: create function dateSample for carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,8 +18,28 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  throw new NotImplementedError('Not implemented');
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(typeof sampleActivity !== 'string'){
+    return false;
+  }
+
+  let MODERN_ACTIVITY = +sampleActivity;
+  if(isNaN(MODERN_ACTIVITY) || MODERN_ACTIVITY < 0){
+    return false;
+  }
+  
+	let HALF_LIFE_PERIOD = 5730;
+  let INITIAL_ACTIVITY = 15;
+
+	let k = 0.693 / HALF_LIFE_PERIOD;
+
+
+	let age = Math.ceil(Math.log(INITIAL_ACTIVITY / MODERN_ACTIVITY) / k);
+  if(age === Infinity || age < 0){
+    return false;
+  }
+	return age;
 }
 
 module.exports = {
