dcd453153744a5067a3036d00c22d7d73e6b8de3
alpekhota <alekspekhota@gmail.com>
2022-05-02 20:40:54 +0200

    feat: add decision to sort-by-height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let sortArr = [];
+
+	for(let i = 0; i < arr.length; i++){
+		console.log(typeof arr[i]);
+		if(arr[i] !== -1){
+			sortArr.push(arr[i]);
+		}
+	}
+	sortArr = sortArr.sort((a,b) => a -b);
+
+	for(let j = 0; j < arr.length; j++){
+		if(arr[j] !== -1){
+			arr[j] = sortArr[0];
+			sortArr.shift();
+
+		}
+	}
+	return arr;
 }
 
 module.exports = {
